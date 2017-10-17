a5dad53c0832d077b82ce25fbc92c9dc69953d6c
sourshane <sdonahue626@gmail.com>
2017-10-17 16:36:23 -0700

    Add moscovium stuff

--- autoscript.sh
+++ autoscript.sh
@@ -553,3 +553,434 @@ mv data/chkaide.sh /usr/sbin/
 
 # Schedule daily file integrity checks
 echo "0 5 * * * root /usr/sbin/chkaide.sh" >> /etc/crontab
+
+
+
+
+
+
+
+
+
+###############
+## MOSCOVIUM ##
+###############
+
+
+echo $'\n[>] Users'
+
+
+#-|-------------- Purge accounts  --------------|-
+
+cp_input_accounts(){
+    # Clear valid_admins and valid_users
+    echo '' > valid_admins
+    echo '' > valid_users
+
+    read -p '      [+] Please enter the valid admins: ' -a admins
+    printf '%s\n' "${admins[@]}" >> data/valid_admins
+    read -p '      [+] Please enter the valid standard users: ' -a users
+    printf '%s\n' "${users[@]}" >> data/valid_users
+} 
+
+cp_purge_accounts(){
+    # All valid admins are also valid users
+    if ! grep -wq -f data/valid_admins data/valid_users; then
+    	cat data/valid_admins >> data/valid_users
+    fi
+
+    # Get system users and admins
+    admins="$(grep -Po '^sudo.+:\K.*$' /etc/group | tr "," "\n")"
+    users="$(cat /etc/passwd | grep bash | awk -F: '{ print $1 }')"
+
+    # Purge users
+    for i in $users; do
+        if grep -Fxqs "$i" 'data/valid_users'; then
+            # If user is authorized
+            chage -E 01/01/2019 -m 5 -M 90 -I 30 -W 14 $i
+            if [ "$i" != "$cp_my_user" ]; then
+                echo "$i:"'CA935_CyberPatriots!' | chpasswd
+                echo "        [+] $i password changed and chage password policy set."
+            fi
+            if [ "$i" = "$cp_my_user" ]; then
+                echo "      [+] $i chage password policy set."
+            fi
+        else
+            if [ "$i" != "root" ]; then
+                read -p "      [?] $i is not an authorized user. Remove them and their files? (y/n) " choice
+                case "$choice" in 
+                  y|Y ) userdel -r $i &>/dev/null && echo "      [+] $i removed.";;
+                esac
+            fi
+        fi
+    done
+
+    # Purge admins
+    for i in $admins; do
+        if ! grep -Fxqs "$i" 'data/valid_admins'; then
+            gpasswd -d $i sudo &>/dev/null
+            echo "    [+] $i is not an authorized admin. $i removed from sudo group."    
+        fi
+    done
+}
+
+read -p "  [?] Edit and correct valid admins and users? (y/n) " choice
+case "$choice" in 
+  y|Y ) read -p "    [?] What is your username? " cp_my_user && cp_input_accounts && cp_create_accounts && cp_purge_accounts;;
+esac
+
+#-|-------------- Lock root account --------------|-
+
+if ! passwd -S | grep -q "root L"; then
+    echo "root:"'$1$FvmieeAj$cDmFLn5RvjYphj3iL1RJZ/' | chpasswd -e
+    passwd -l root 2>&1>/dev/null
+    echo "  [+] Root account locked."
+fi
+
+
+#-|-------------- lightdm Config --------------|-
+
+cp /etc/lightdm/lightdm.conf data/backup_files/lightdm.conf
+cp -f data/references/lightdm.conf /etc/lightdm/lightdm.conf
+echo "  [+] Lightdm file secured."
+
+
+#-|-------------- sudoers Config --------------|-
+
+cp /etc/sudoers data/backup_files/sudoers.backup
+cp -f data/references/sudoers /etc/sudoers
+echo "  [+] Sudoers file secured."
+
+
+#-|-------------- Password Policy --------------|-
+
+if ! dpkg -s libpam-cracklib >/dev/null 2>&1; then
+    echo "  [+] Installing libpam-cracklib..." &&
+    apt-get -qq -y install libpam-cracklib
+fi
+
+cp /etc/login.defs data/backup_files/login.defs.backup
+cp -f data/references/login.defs /etc/login.defs
+
+cp /etc/pam.d/common-password data/backup_files/common-password.backup
+cp -f data/references/common-password /etc/pam.d/common-password
+
+cp /etc/pam.d/common-auth data/backup_files/common-auth.backup
+cp -f data/references/common-auth /etc/pam.d/common-auth
+
+echo "  [+] Password policy set."
+
+#-|-------------- audtid Policy --------------|-
+
+if ! dpkg -s auditd >/dev/null 2>&1; then
+    echo "  [+] Installing auditd..." &&
+    apt-get -qq -y install auditd
+    auditctl -e 1 &>/dev/null
+    echo "    [+] Audit policy set with auditd."
+fi
+
+echo $'\n[>] Networking'
+
+
+#-|-------------- Hosts File --------------|-
+
+echo '127.0.0.1' $HOSTNAME >> data/references/hosts
+cp /etc/hosts data/backup_files/hosts.backup
+cp -f data/references/hosts /etc/hosts
+echo "  [+] Cleaned hosts file."
+
+
+#-|-------------- SSHD Config --------------|-
+
+cp /etc/ssh/ssh_config data/backup_files/ssh_config.backup
+cp -f data/references/ssh_config /etc/ssh/ssh_config
+
+cp /etc/ssh/sshd_config data/backup_files/sshd_config.backup
+cp -f data/references/sshd_config /etc/ssh/sshd_config
+echo "  [+] Secured ssh settings."
+
+
+#-|-------------- Firewall --------------|-
+
+if ! dpkg -s ufw >/dev/null 2>&1; then 
+    echo "    [+] Installing ufw..."
+    apt-get -qq -y install ufw
+fi
+echo "y" | ufw reset >/dev/null
+ufw default deny >/dev/null 2>&1
+ufw logging on >/dev/null 2>&1
+
+services_length="$(sed -n '$=' data/valid_admins)"
+for ((i=1; i<=services_length; i++)); do
+	service="$(awk 'FNR == $i { print; exit }' data/critical_services | tr '[:lower:]' '[:upper:]')"
+	ufw allow $service >/dev/null 2>&1
+	if [ "$service" = "SSH" ]; then
+		ufw limit SSH >/dev/null 2>&1
+	fi
+done
+ufw enable >/dev/null 2>&1
+echo "  [+] Firewall configured."
+
+
+#-|-------------- Ports? --------------|-
+
+cp_ports(){
+    echo $'\n[>] Open Ports'
+
+    # Shows all listening ports, as well as the services running on them. If
+    # the service isn't required, you should remove it.
+
+    rm ./open_ports 2>&1>/dev/null
+    echo "   [+] Open ports:"
+    netstat -tulpnwa | grep 'LISTEN\|ESTABLISHED' | grep -v "tcp6\|udp6" | awk '{ print $4 " - " $7 }' | awk -F: '{ print "	IPV4 - " $2 }' >> ./open_ports
+    netstat -tulpnwa | grep 'LISTEN\|ESTABLISHED' | grep "tcp6\|udp6" | awk '{ print $4 " - " $7 }' | awk -F: '{ print "	IPV6 - " $4 }' >> ./open_ports
+
+    while read l; do
+        echo $l
+        pid=$(echo $l | awk '{ print $5 }' | awk -F/ '{ print $1 }')
+        #printf "\tRunning from: $(ls -la /proc/$pid/exe | awk '{ print $11 }')\n"
+        command="$(cat /proc/$pid/cmdline | sed 's/\x0/ /g' | sed 's/.$//')"
+        #echo "$command"
+        if [[ "$command" == *"nc -l"* ]]; then
+            for i in $(grep -s -r --exclude-dir={proc,lib,tmp,usr,var,libproc,sys,run,dev} "$command" $(ls -l /proc/$pid/cwd | awk '{ print $11 }') | awk -F: '{ print $1 }'); do
+                printf "   [!]  $i\n"
+            done
+        fi
+    done < ./open_ports | sed 's/^/        /' 
+
+    # Monitor network
+
+    echo $'\n[>] Listening Network Connections'
+    netstat -ntulp | sed 's/^/        /' 
+}
+
+
+#-|-------------- apache2 Config --------------|-
+
+if [ -e /etc/apache2/apache2.conf ]; then
+	echo '<Directory>' >> /etc/apache2/apache2.conf
+	echo -e ' \t AllowOverride None' >> /etc/apache2/apache2.conf
+	echo -e ' \t Order Deny,Allow' >> /etc/apache2/apache2.conf
+	echo -e ' \t Deny from all' >> /etc/apache2/apache2.conf
+	echo '<Directory/>' >> /etc/apache2/apache2.conf
+	echo UserDir disabled root >> /etc/apache2/apache2.conf
+	echo "  [+] apache2 configured."
+fi
+
+
+#-|-------------- Miscellaneous Network Settings --------------|-
+
+# SYN Cookie Protection
+if grep -q 0 /proc/sys/net/ipv4/tcp_syncookies; then 
+	echo 1 > /proc/sys/net/ipv4/tcp_syncookies
+	echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
+	echo "  [+] SYN cookie protection enabled."
+fi
+
+# Disable IPv6
+if grep -q 0 /proc/sys/net/ipv6/conf/all/disable_ipv6; then
+	echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+	echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
+	echo "  [+] IPv6 Disabled."
+fi 
+
+# Don't act as router
+(sysctl -w net.ipv4.ip_forward=0
+sysctl -w net.ipv4.conf.all.send_redirects=0
+sysctl -w net.ipv4.conf.default.send_redirects=0 )  &>/dev/null
+
+# Make sure no one can alter the routing tables
+(sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv4.conf.default.accept_redirects=0
+sysctl -w net.ipv4.conf.all.secure_redirects=0
+sysctl -w net.ipv4.conf.default.secure_redirects=0)  &>/dev/null
+
+
+
+echo $'\n[>] Software'
+
+
+#-|-------------- Cron --------------|-
+
+read -p "  [?] Check for running cron jobs? (y/n) " choice
+case "$choice" in 
+  y|Y ) for user in $(cut -f1 -d: /etc/passwd); do crontab -u $user -l; done
+esac
+
+
+#-|-------------- apt Sources --------------|-
+
+cp /etc/apt/sources.list data/backup_files/sources.list.backup
+cp -f data/references/sources.list /etc/apt/sources.list
+echo "  [+] Cleaned apt sources."
+
+
+#-|-------------- Config Security --------------|-
+
+cp /etc/resolv.conf data/backup_files/resolv.conf
+cp -f data/references/resolv.conf /etc/resolv.conf
+
+cp /etc/rc.local data/backup_files/rc.local.backup
+cp -f data/references/rc.local /etc/rc.local
+echo "  [+] Secured certain configuration files."
+
+
+#-|-------------- Unauthorized Files --------------|-
+
+echo "" > 'data/unauthorized_media'
+(find /home -name "*.mp3"
+find /home -name "*.wav"
+find /home -name "*.wma"
+find /home -name "*.aac"
+find /home -name "*.mp4"
+find /home -name "*.mov"
+find /home -name "*.avi"
+find /home -name "*.gif"
+find /home -name "*.jpg"
+find /home -name "*.jpeg"
+find /home -name "*.png"
+find /home -name "*.bmp"
+find /home -name "*.exe"
+find /home -name "*.msi"
+find /home -name "*.bat"
+find /home -name "*.sh") >> 'data/unauthorized_media'
+
+# Remove authorized media files from list
+for i in $(echo "CP-IX "; echo "ubuntu-14"; echo ".cache"; echo "Trash"); do
+    sed -i "/$i/d" 'data/unauthorized_media'
+done
+
+# Delete unauthorized media
+unauth_media="$(cat data/unauthorized_media)"
+if [ -n "$unauth_media" ]; then
+	echo "  [>] Unauthorized files"
+	cat 'data/unauthorized_media' | sed 's/^/  /'
+	read -p $'\n  [?] Delete all unauthorized media? (y/n) ' choice
+	case "$choice" in
+	y|Y ) sed -i '/[>]/d' 'data/unauthorized_media' &&
+		sed -i '/^$/d' 'data/unauthorized_media' &&
+		xargs -0 rm -r < <(tr \\n \\0 <'data/unauthorized_media')&>/dev/null || true &&
+		echo "    [+] Unauthorized media deleted.";;
+	esac
+fi
+
+
+#-|-------------- Unwanted Programs --------------|-
+
+unwanted_programs="$(dpkg --get-selections | grep -E '^(apache|cupsd|master|nginx|nmap|medusa|john|nikto|hydra|tightvnc|bind|vsftpd|netcat)' | grep -v 'bind9-host' | grep -v 'deinstall')"
+if [ -n "$unwanted_programs" ]; then
+    echo "  [+] Potentially unwanted programs:"
+    echo "$unwanted_programs" | grep -o '^\S*' > data/uninstalled_packages
+    cat data/uninstalled_packages | sed 's/^/    /'
+    read -p "  [?] Remove all these programs? (y/n) " choice
+    case "$choice" in
+    y|Y ) apt-get purge --auto-remove $(<'data/uninstalled_packages') && echo "  [+] Unwanted programs removed.";;
+    esac
+fi
+
+#-|-------------- Unwanted Services --------------|-
+
+cp_purge_services(){
+  echo "    [+] Potentially unwanted services:"
+	echo "$services_to_delete" | sed 's/^/      /'
+	read -p "    [?] Disable these services? (y/n) " choice
+	case "$choice" in
+	y|Y ) while read -r s; do
+        service $s stop
+        update-rc.d $s disable
+      done <<< "$services_to_delete"
+	 ;;
+	esac
+}
+
+cp_verify_services(){
+  # Spacing is important here
+  while read -r s; do
+    if pgrep $s >/dev/null 2>&1; then
+      if [ -n "$services_to_delete" ]; then
+        services_to_delete="${services_to_delete}
+${s}"
+      else
+        services_to_delete="${services_to_delete}${s}"
+      fi
+    fi
+  done <<< "$unwanted_services"
+
+  # Make the newline count
+  unwanted_services="$(echo -e "$services_to_delete")"
+}
+
+unwanted_services="$(service --status-all |& grep -wEo '(mysqld|postgres|dovecot|exim4|postfix|nfs|nmbd|rpc.mountd|rpc.nfsd|smbd|vsftpd|mpd|bind|dnsmasq|xinetd|inetd|telnet|cupsd|saned|ntpd|cron|apache2|httpd|jetty|nginx|tomcat)' | grep -v $(<data/critical_services) | grep -v "[ - ]")"
+services_to_delete="$(echo '')"
+
+# Because -service --status-all is trash, verify if services are running
+cp_verify_services
+
+if [ -n "$unwanted_services" ]; then
+  read -p $'  [?] Purge unwanted services? (y/n) ' choice
+  case "$choice" in
+    y|Y ) read -p $'    [?] Edit critical services? (y/n) ' choice
+          case "$choice" in
+          y|Y ) nano data/critical_services && cp_purge_services;;
+          * ) cp_purge_services;;
+          esac;
+  esac
+fi
+
+
+
+#-|-------------- Updates --------------|-
+
+# Update system
+read -p "  [?] Update/upgrade the system/distro? (y/n) " choice
+case "$choice" in
+  y|Y ) apt-get -y update && apt-get -y upgrade && apt-get dist-upgrade && apt-get -y install firefox && echo "  [+] System upgraded.";;
+esac
+
+
+# Check for updates daily
+if ! grep -q "APT::Periodic::Update-Package-Lists \"1\";" /etc/apt/apt.conf.d/10periodic; then
+    sed -i 's/APT::Periodic::Update-Package-Lists "0";/APT::Periodic::Update-Package-Lists "1";/g' /etc/apt/apt.conf.d/10periodic
+    echo "  [+] Daily updates configured."
+fi
+
+
+#-|-------------- Media Codecs --------------|-
+
+if ! dpkg -s gstreamer1.0-plugins-good >/dev/null 2>&1; then
+  read -p $'\n  [?] Install media codecs? (y/n) ' choice
+  case "$choice" in
+  y|Y )  echo "  [+] Installing media codecs..." &&
+         apt-get -qq -y install gstreamer1.0-plugins-good ubuntu-restricted-extras
+  esac
+fi
+
+
+#-|------------------ Scans -----------------|-
+
+# First scanner
+read -p "  [?] Scan with lynis? (y/n) " choice
+case "$choice" in
+y|Y ) if ! dpkg -s lynis >/dev/null 2>&1; then echo "    [+] Installing lynis..." &&
+apt-get -qq -y install lynis; fi && echo $'\n    [+] Scanning with lynis...' &&
+lynis -Q
+esac
+
+# Second scanner
+read -p "  [?] Scan with chkrootkit? (y/n) " choice
+case "$choice" in
+y|Y ) if ! dpkg -s chkrootkit >/dev/null 2>&1; then echo "    [+] Installing chkrootkit..." &&
+apt-get -qq -y install chkrootkit; fi && echo $'\n    [+] Scanning with chkrootkit...' &&
+chkrootkit
+esac
+
+# Third scanner
+read -p "  [?] Scan with rkhunter? (y/n) " choice
+case "$choice" in
+y|Y ) if ! dpkg -s rkhunter >/dev/null 2>&1; then echo "    [+] Installing rkhunter..." &&
+apt-get -qq -y install rkhunter; fi && echo $'\n    [+] Scanning with rkhunter...' &&
+rkhunter -c
+esac
+
+
+
