561f73f956d0453bf79ed54a02718be414f10b92
ArtificialAmateur <20297606+ArtificialAmateur@users.noreply.github.com>
2017-10-17 19:49:01 -0500

    Add moscovium features

--- autoscript.sh
+++ autoscript.sh
@@ -1,9 +1,70 @@
 #!/bin/bash
 
+#####################
+## User Management ##
+#####################
+input_accounts(){
+    # Clear valid_admins and valid_users
+    echo '' > valid_admins
+    echo '' > valid_users
+
+    read -p '      [+] Please enter the valid admins: ' -a admins
+    printf '%s\n' "${admins[@]}" >> data/valid_admins
+    read -p '      [+] Please enter the valid standard users: ' -a users
+    printf '%s\n' "${users[@]}" >> data/valid_users
+} 
+
+purge_accounts(){
+    # All valid admins are also valid users
+    if ! grep -wq -f data/valid_admins data/valid_users; then
+    	cat data/valid_admins >> data/valid_users
+    fi
+
+    # Get system users and admins
+    admins="$(grep -Po '^sudo.+:\K.*$' /etc/group | tr "," "\n")"
+    users="$(sed -n 's/^\([^:]*\).*sh$/\1/p' /etc/passwd)"
+
+    # Purge users
+    for i in $users; do
+        if grep -Fxqs "$i" 'data/valid_users'; then
+            # If user is authorized
+            chage -E 01/01/2019 -m 5 -M 90 -I 30 -W 14 $i
+            if [ "$i" != "$my_user" ]; then
+                echo "$i:"'MyExamplePassword' | chpasswd
+                echo "        [+] $i password changed and chage password policy set."
+            fi
+            if [ "$i" = "$my_user" ]; then
+                echo "      [+] $i chage password policy set."
+            fi
+        else
+            if [ "$i" != "root" ]; then
+                read -p "      [?] $i is not an authorized user. Remove them and their files? (y/n) " choice
+                case "$choice" in 
+                  y|Y ) userdel -r $i &>/dev/null && echo "      [+] $i removed.";;
+                esac
+            fi
+        fi
+    done
+
+    # Purge admins
+    for i in $admins; do
+        if ! grep -Fxqs "$i" 'data/valid_admins'; then
+            gpasswd -d $i sudo &>/dev/null
+            echo "    [+] $i is not an authorized admin. $i removed from sudo group."    
+        fi
+    done
+}
+
+read -p "  [?] Edit and correct valid admins and users? (y/n) " choice
+case "$choice" in 
+  y|Y ) read -p "    [?] What is your username? " my_user && input_accounts && create_accounts && purge_accounts;;
+esac
+
 ##########################
 ## Disable Root Account ##
 ##########################
 echo "  [+] Disabling root account..."
+echo "root:"'$1$FvmieeAj$cDmFLn5RvjYphj3iL1RJZ/' | chpasswd -e
 sed -i "s|root:x:0:0:root:/root:/bin/bash|root:x:0:0:root:/root:/sbin/nologin|g" /etc/passwd
 echo > /etc/securetty
 
@@ -27,7 +88,7 @@ EOF
 ## Enforce GPG Check ##
 #######################
 echo "  [+] Enforcing GPG check..."
-if grep 'gpgcheck=0'; then
+if grep 'gpgcheck=0' /etc/yum.conf; then
     sed -i 's/gpgcheck=0/gpgcheck=1/g' /etc/yum.conf
 fi
 
@@ -110,29 +171,22 @@ chkconfig tcpmux-server off
 ######################
 ## Set Daemon Umask ##
 ######################
+echo "  [+] Setting up umask..."
 echo "umask 027" >> /etc/sysconfig/init
-
-
-############################
-## Remove X Window System ##
-############################
-cd /etc/lib/systemd/system/
-unlink default.target
-ln -s /usr/lib/systemd/system/multi-user.target default.target
-yum remove xorg-x11-server-common -y
 
 
 #############################
 ## Remove/Disable Services ##
 #############################
+echo "  [+] Removing unwanted programs..."
 systemctl disable avahi-daemon cups nfslock rpcgssd rpcbind rpcidmapd rpcsvcgssd
-
 yum erase dhcp openldap-servers openldap-clients bind vsftpd httpd dovecot samba squid net-snmp telnet-server telnet rsh-server rsh ypbind ypserv tftp tftp-server talk talk-server xinetd -y
 
 
 #############
 ## Fix NTP ##
 #############
+echo "  [+] Setting up NTP..."
 yum install ntp -y
 cat > /etc/ntp.conf <<-EOF
 driftfile /var/lib/ntp/drift
@@ -148,23 +202,17 @@ includefile /etc/ntp/crypto/pw
 keys /etc/ntp/keys
 disable monitor
 EOF
-
 echo "OPTIONS=\"-u ntp:ntp\"" >> /etc/sysconfig/ntpd
-
 systemctl start ntpd
 systemctl enable ntpd
-
-
-####################################
-## Set Postfix to Local-Only Mode ##
-####################################
-sed -i 's/^inet_interfaces .*$/inet_interfaces = localhost/g' /etc/postfix/main.cf
-systemctl restart postfix
 
 
 ###########################
 ## Network Configuration ##
 ###########################
+echo "  [+] SYN cookie protection enabled."
+echo "  [+] IPv6 Disabled."
+echo "  [+] Routing Disabled."
 echo "net.ipv4.ip_forward=0
 net.ipv4.conf.all.send_redirects=0
 net.ipv4.conf.default.send_redirects=0
@@ -180,48 +228,47 @@ net.ipv4.icmp_echo_ignore_broadcasts=1
 net.ipv4.icmp_ignore_bogus_error_responses=1
 net.ipv4.conf.all.rp_filter=1
 net.ipv4.conf.default.rp_filter=1
-net.ipv4.tcp_syncookies=1
+net.ipv4.tsyncookies=1
+net.ipv6.conf.all.disable_ipv6=1
 net.ipv6.conf.all.accept_ra=0
 net.ipv6.conf.default.accept_ra=0
 net.ipv6.conf.all.accept_redirects=0
 net.ipv6.conf.default.accept_redirects=0" >> /etc/sysctl.conf
 
-/sbin/sysctl -w net.ipv4.ip_forward=0
-/sbin/sysctl -w net.ipv4.conf.all.send_redirects=0
-/sbin/sysctl -w net.ipv4.conf.default.send_redirects=0
-/sbin/sysctl -w net.ipv4.conf.all.accept_source_route=0
-/sbin/sysctl -w net.ipv4.conf.default.accept_source_route=0
-/sbin/sysctl -w net.ipv4.conf.all.accept_redirects=0
-/sbin/sysctl -w net.ipv4.conf.default.accept_redirects=0
-/sbin/sysctl -w net.ipv4.conf.all.secure_redirects=0
-/sbin/sysctl -w net.ipv4.conf.default.secure_redirects=0
-/sbin/sysctl -w net.ipv4.conf.all.log_martians=1
-/sbin/sysctl -w net.ipv4.conf.default.log_martians=1
-/sbin/sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
-/sbin/sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
-/sbin/sysctl -w net.ipv4.conf.all.rp_filter=1
-/sbin/sysctl -w net.ipv4.conf.default.rp_filter=1
-/sbin/sysctl -w net.ipv4.tcp_syncookies=1
-/sbin/sysctl -w net.ipv4.route.flush=1
-/sbin/sysctl -w net.ipv6.conf.all.accept_ra=0
-/sbin/sysctl -w net.ipv6.conf.default.accept_ra=0
-/sbin/sysctl -w net.ipv6.conf.all.accept_redirects=0
-/sbin/sysctl -w net.ipv6.conf.default.accept_redirects=0
-/sbin/sysctl -w net.ipv6.route.flush=1
+sysctl -w net.ipv4.ip_forward=0
+sysctl -w net.ipv4.conf.all.send_redirects=0
+sysctl -w net.ipv4.conf.default.send_redirects=0
+sysctl -w net.ipv4.conf.all.accept_source_route=0
+sysctl -w net.ipv4.conf.default.accept_source_route=0
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv4.conf.default.accept_redirects=0
+sysctl -w net.ipv4.conf.all.secure_redirects=0
+sysctl -w net.ipv4.conf.default.secure_redirects=0
+sysctl -w net.ipv4.conf.all.log_martians=1
+sysctl -w net.ipv4.conf.default.log_martians=1
+sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
+sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
+sysctl -w net.ipv4.conf.all.rp_filter=1
+sysctl -w net.ipv4.conf.default.rp_filter=1
+sysctl -w net.ipv4.tsyncookies=1
+sysctl -w net.ipv4.route.flush=1
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.all.accept_ra=0
+sysctl -w net.ipv6.conf.default.accept_ra=0
+sysctl -w net.ipv6.conf.all.accept_redirects=0
+sysctl -w net.ipv6.conf.default.accept_redirects=0
+sysctl -w net.ipv6.route.flush=1
 
-yum install tcp_wrappers -y
+yum install twrappers -y
 
 
-#############################
-## Create /etc/hosts.allow ##
-#############################
+#####################
+## Configure hosts ##
+#####################
+# Create /etc/hosts.allow
 echo "ALL: 10.1.1.0/255.255.255.0" >> /etc/hosts.allow
 chmod 644 /etc/hosts.allow
-
-
-############################
-## Create /etc/hosts.deny ##
-############################
+# Create /etc/hosts.deny
 echo "ALL: ALL" >> /etc/hosts.deny
 chmod 644 /etc/hosts.deny
 
@@ -246,14 +293,11 @@ systemctl start firewalld
 ####################
 ## Install Syslog ##
 ####################
+echo "  [+] Configuring syslog..."
 yum install rsyslog -y
 systemctl enable rsyslog
 systemctl start rsyslog
-
-
-######################
-## Configure Syslog ##
-######################
+# Configure Syslog
 cat >/etc/rsyslog.conf <<-EOF
 \$ModLoad imuxsock
 \$ModLoad imjournal
@@ -287,6 +331,7 @@ systemctl restart rsyslog
 ########################
 ## Configure Auditing ##
 ########################
+echo "  [+] Configuring auditd..."
 systemctl start auditd
 systemctl enable auditd
 
@@ -311,47 +356,35 @@ admin_space_left = 50
 admin_space_left_action = halt
 disk_full_action = SUSPEND
 disk_error_action = SUSPEND
-##tcp_listen_port =
-tcp_listen_queue = 5
-tcp_max_per_addr = 1
-##tcp_client_ports = 1024-65535
-tcp_client_max_idle = 0
+##tlisten_port =
+tlisten_queue = 5
+tmax_per_addr = 1
+##tclient_ports = 1024-65535
+tclient_max_idle = 0
 enable_krb5 = no
 krb5_principal = auditd
 ##krb5_key_file = /etc/audit/audit.key
 EOF
 
-
-##############################################################
-## Enable Auditing for Processes that Start Prior to auditd ##
-##############################################################
+# Enable Auditing for Processes that Start Prior to auditd
 echo 'GRUB_CMDLINE_LINUX="audit=1"' >> /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
-
-#########################################################
-## Record Events that Modify Date and Time Information ##
-#########################################################
+# Record Events that Modify Date and Time Information
 echo "-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change 
 -a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change
 -a always,exit -F arch=b64 -S clock_settime -k time-change 
 -a always,exit -F arch=b32 -S clock_settime -k time-change 
 -w /etc/localtime -p wa -k time-change" >> /etc/audit/rules.d/audit.rules
 
-
-######################################################
-## Record Events that Modify User/Group Information ##
-######################################################
+# Record Events that Modify User/Group Information
 echo "-w /etc/group -p wa -k identity 
 -w /etc/passwd -p wa -k identity 
 -w /etc/gshadow -p wa -k identity 
 -w /etc/shadow -p wa -k identity 
 -w /etc/security/opasswd -p wa -k identity" >> /etc/audit/rules.d/audit.rules
 
-
-############################################################
-## Record Events that Modify System's Network Environment ##
-############################################################
+# Record Events that Modify System's Network Environment
 echo "-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale 
 -a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale 
 -w /etc/issue -p wa -k system-locale 
@@ -359,32 +392,20 @@ echo "-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-local
 -w /etc/hosts -p wa -k system-locale 
 -w /etc/sysconfig/network -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
 
-
-#########################################################
-## Record Events that Modify Mandatory Access Controls ##
-#########################################################
+# Record Events that Modify Mandatory Access Controls
 echo "-w /etc/selinux/ -p wa -k MAC-policy" >> /etc/audit/rules.d/audit.rules
 
-
-#####################################
-## Collect Login and Logout Events ##
-#####################################
+# Collect Login and Logout Events #
 echo "-w /var/log/faillog -p wa -k logins 
 -w /var/log/lastlog -p wa -k logins 
 -w /var/log/tallylog -p wa -k logins" >> /etc/audit/rules.d/audit.rules
 
-
-############################################
-## Collect Session Initiation Information ##
-############################################
+# Collect Session Initiation Information
 echo "-w /var/run/utmp -p wa -k session 
 -w /var/log/wtmp -p wa -k session 
 -w /var/log/btmp -p wa -k session" >> /etc/audit/rules.d/audit.rules
 
-
-#########################################################################
-## Collect Discretionary Access Control Permission Modification Events ##
-#########################################################################
+# Collect Discretionary Access Control Permission Modification Events
 echo "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod 
 -a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod 
 -a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod 
@@ -392,61 +413,36 @@ echo "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F
 -a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod 
 -a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/rules.d/audit.rules
 
-
-###############################################################
-## Collect Unsuccessful Unauthorised Access Attemps to Files ##
-###############################################################
+# Collect Unsuccessful Unauthorised Access Attemps to Files
 echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access 
 -a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access 
 -a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access 
 -a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access" >> /etc/audit/rules.d/audit.rules
 
+# Collect Use of Privileged Commands
 
-########################################
-## Collect Use of Privileged Commands ##
-########################################
-
-
-###########################################
-## Collect Successful File System Mounts ##
-###########################################
+# Collect Successful File System Mounts
 echo "-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts 
 -a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> /etc/audit/rules.d/audit.rules
 
-
-##########################################
-## Collect File Deletion Events by User ##
-##########################################
+# Collect File Deletion Events by User
 echo "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete 
 -a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> /etc/audit/rules.d/audit.rules
 
-
-####################################################
-## Collect Changes to System Administration Scope ##
-####################################################
+# Collect Changes to System Administration Scope
 echo "-w /etc/sudoers -p wa -k scope" >> /etc/audit/rules.d/audit.rules
 
-
-##########################################
-## Collect System Administrator Actions ##
-##########################################
+# Collect System Administrator Actions
 echo "-w /var/log/sudo.log -p wa -k actions" >> /etc/audit/rules.d/audit.rules
 
-
-#################################################
-## Collect Kernel Module Loading and Unloading ##
-#################################################
+# Collect Kernel Module Loading and Unloading
 echo "-w /sbin/insmod -p x -k modules 
 -w /sbin/rmmod -p x -k modules 
 -w /sbin/modprobe -p x -k modules
 -a always,exit -F arch=b64 -S init_module -S delete_module -k modules" >> /etc/audit/rules.d/audit.rules
 
-
-############################################
-## Make the Audit Configuration Immutable ##
-############################################
+# Make the Audit Configuration Immutable
 echo "-e 2" >> /etc/audit/rules.d/audit.rules
-
 pkill -HUP -P 1 auditd
 
 
@@ -456,28 +452,17 @@ pkill -HUP -P 1 auditd
 yum install logrotate -y
 
 
-####################
-## Enable Anacron ##
-####################
-yum install cronie-anacron -y
-
-
 ##################
 ## Enable Crond ##
 ##################
+echo "  [+] Enabling cron..."
 systemctl enable crond
-
-
-###########################################
-## Set User/Group Permissions on Anacron ##
-###########################################
-chown root:root /etc/anacrontab
-chmod og-rwx /etc/anacrontab
 
 
 ########################################
 ## Set User/Group Permissions on Cron ##
 ########################################
+echo "  [+] Restricting crontab permissions..."
 chown root:root /etc/crontab
 chmod og-rwx /etc/crontab
 chown root:root /etc/cron.hourly
@@ -495,6 +480,7 @@ chmod og-rwx /etc/cron.d
 ########################
 ## Restrict At Daemon ##
 ########################
+echo "  [+] Restricting daemon permissions..."
 rm -f /etc/at.deny
 rm -f /etc/cron.deny
 touch /etc/at.allow
@@ -508,19 +494,9 @@ chmod og-rwx /etc/cron.allow
 #######################
 ## SSH Configuration ##
 #######################
-sed -i 's/^Protocol .*$/Protocol 2/g' /etc/ssh/sshd_config
-sed -i 's/^LogLevel .*$/LogLevel INFO/g' /etc/ssh/sshd_config
-sed -i 's/X11Forwarding yes/X11Forwarding no/g' /etc/ssh/sshd_config
-sed -i 's/^MaxAuthTries .*$/MaxAuthTries 4/g' /etc/ssh/sshd_config
-sed -i 's/IgnoreRhosts no/IgnoreRhosts yes/g' /etc/ssh/sshd_config
-sed -i 's/HostbasedAuthentication yes/HostbasedAuthentication no/g' /etc/ssh/sshd_config
-sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-sed -i 's/PermitEmptyPasswords yes/PermitEmptyPasswords no/g' /etc/ssh/sshd_config
-sed -i 's/PermitUserEnvironment yes/PermitUserEnvironment no/g' /etc/ssh/sshd_config
-sed -i 's/^Ciphers .*$/Ciphers aes128-ctr,aes192-ctr,aes256-ctr/g' /etc/ssh/sshd_config
-sed -i 's/^ClientAliveInterval .*$/ClientAliveInterval 300/g' /etc/ssh/sshd_config
-sed -i 's/^ClientAliveCountMax .*$/ClientAliveCountMax 0/g' /etc/ssh/sshd_config
-
+echo "  [+] Configuring SSH..."
+cat data/ssh_config > /etc/ssh/ssh_config
+cat data/sshd_config > /etc/ssh/sshd_config
 chown root:root /etc/ssh/sshd_config
 chmod 600 /etc/ssh/sshd_config
 
@@ -530,7 +506,7 @@ chmod 600 /etc/ssh/sshd_config
 ###################
 echo "  [+] Configuring password policy..."
 authconfig --passalgo=sha512 --update
-users="$(cat /etc/passwd | grep 'bash\|zsh\|ksh\|fish\|tcsh\|csh' | awk -F: '{ print $1 }')"
+users="$(sed -n 's/^\([^:]*\).*sh$/\1/p' /etc/passwd)"
 for i in $users; do
     chage -E 01/01/2019 -m 5 -M 90 -I 30 -W 14 $i
 done
@@ -550,6 +526,5 @@ echo "  [+] Enabling AIDE..."
 yum install aide -y
 aide --init -B 'database_out=file:/var/lib/aide/aide.db.gz'
 mv data/chkaide.sh /usr/sbin/
-
 # Schedule daily file integrity checks
-echo "0 5 * * * root /usr/sbin/chkaide.sh" >> /etc/crontab
+echo "*/15 * * * * root /usr/sbin/chkaide.sh" >> /etc/crontab
